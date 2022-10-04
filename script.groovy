8153ff268f674fafc5c87353aef18bed8f5e5527
acruzfg <112987246+acruzfg@users.noreply.github.com>
2022-10-04 13:21:30 -0500

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def BuildGift() {
+    echo 'Building the gift ....'
+}
+
+def TestApp() {
+    echo 'Dont know if its tough enough ....'
+}
+
+def DepApp() {
+    echo 'Sending the gift by FEDEX ....'
+}
+
+return this 
